09b06e369074d39fdef83c9480b968eed280f160
nousi <nousidayo@gmail.com>
2020-11-25 23:38:26 +0900

    Update route in api.php

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::group(['middleware' => ['auth:airlock']], function () {
+    Route::get('user', function (Request $request) {
+        return response()->json(['user' => $request->user()]);
+    });
+    Route::post('logout', 'Auth\Api\LoginController@logout')->name('api.logout');
 });
+
+Route::post('register', 'Auth\Api\RegisterController@register')->name('api.register');
+Route::post('login', 'Auth\Api\LoginController@login')->name('api.login');
+
 Route::resource('tasks', 'TaskController');
