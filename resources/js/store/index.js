a6fdc9ef9787623b07eef03a2e33d90da1781bbe
nousi <nousidayo@gmail.com>
2020-11-26 11:00:48 +0900

    const store in index.js

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,3 +1,12 @@
 import Vue from "vue";
-import Vuex from 'vuex'
+import Vuex from "vuex";
 import auth from "./auth";
+
+Vue.use(Vuex);
+const store = new View.Store({
+    modeles: {
+        auth
+    }
+});
+
+export default store;
