7b87baa814683d0a8254beddaea8d632dedcda8e
nousi <nousidayo@gmail.com>
2021-01-17 22:47:11 +0900

    getTasks() {を更新
    getTasks() {
          $http
            .get("/api/tasks", null, {
              headers: {
                Authorization: `Bearer ${state.token}`
              }
            })

--- resources/js/components/TaskListComponent.vue
+++ resources/js/components/TaskListComponent.vue
@@ -1,78 +1,79 @@
 <template>
-    <div class="container">
-        <table class="table table-hover">
-            <thead class="thead-light">
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Content</th>
-                    <th scope="col">author</th>
-                    <th scope="col">Show</th>
-                    <th scope="col">Edit</th>
-                    <th scope="col">Delete</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="task in tasks">
-                    <th scope="row">{{ task.id }}</th>
-                    <td>{{ task.title }}</td>
-                    <td>{{ task.content }}</td>
-                    <td>{{ task.author }}</td>
-                    <td>
-                        <router-link
-                            v-bind:to="{
+  <div class="container">
+    <table class="table table-hover">
+      <thead class="thead-light">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Title</th>
+          <th scope="col">Content</th>
+          <th scope="col">author</th>
+          <th scope="col">Show</th>
+          <th scope="col">Edit</th>
+          <th scope="col">Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="task in tasks">
+          <th scope="row">{{ task.id }}</th>
+          <td>{{ task.title }}</td>
+          <td>{{ task.content }}</td>
+          <td>{{ task.author }}</td>
+          <td>
+            <router-link
+              v-bind:to="{
                                 name: 'task.show',
                                 params: { taskId: task.id }
                             }"
-                        >
-                            <button class="btn btn-primary">show</button>
-                        </router-link>
-                    </td>
-                    <td>
-                        <router-link
-                            v-bind:to="{
+            >
+              <button class="btn btn-primary">show</button>
+            </router-link>
+          </td>
+          <td>
+            <router-link
+              v-bind:to="{
                                 name: 'task.edit',
                                 params: { taskId: task.id }
                             }"
-                        >
-                            <button class="btn btn-success">Edit</button>
-                        </router-link>
-                    </td>
-                    <td>
-                        <button
-                            class="btn btn-danger"
-                            v-on:click="deleteTask(task.id)"
-                        >
-                            Delete
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+            >
+              <button class="btn btn-success">Edit</button>
+            </router-link>
+          </td>
+          <td>
+            <button class="btn btn-danger" v-on:click="deleteTask(task.id)">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
-    data: function() {
-        return {
-            tasks: []
-        };
+  data: function() {
+    return {
+      tasks: []
+    };
+  },
+  methods: {
+    getTasks() {
+      $http
+        .get("/api/tasks", null, {
+          headers: {
+            Authorization: `Bearer ${state.token}`
+          }
+        })
+        .then(res => {
+          this.tasks = res.data;
+        });
     },
-    methods: {
-        getTasks() {
-            axios.get("/api/tasks").then(res => {
-                this.tasks = res.data;
-            });
-        },
-        deleteTask(id) {
-            axios.delete("/api/tasks/" + id).then(res => {
-                this.getTasks();
-            });
-        }
-    },
-    mounted() {
+    deleteTask(id) {
+      $http.delete("/api/tasks/" + id).then(res => {
         this.getTasks();
+      });
     }
+  },
+  mounted() {
+    this.getTasks();
+  }
 };
 </script>
